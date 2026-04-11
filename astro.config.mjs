054945520e72debbccf43e048607e778aaa40894
Jhosua <168266147+Jhosua-Lascarro@users.noreply.github.com>
2026-04-11 11:20:01 -0500

    feat: add landing UI foundation and shared components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,30 @@ import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: 'https://www.agrochoco.com',
     vite: {
         plugins: [tailwindcss()]
     },
     fonts: [
         {
-            name: 'Roboto',
-            cssVariable: '--font-roboto',
+            // Headings & Titles
+            name: 'Plus Jakarta Sans',
+            cssVariable: '--font-jakarta',
             provider: fontProviders.google(),
-            weights: [400],
-            styles: ["normal", "italic"],
+            weights: [600, 800], // Semibold y ExtraBold
+            styles: ["normal"],
             subsets: ["latin"],
             fallbacks: ["sans-serif"],
-            formats: ["woff2"],
+        },
+        {
+            // Body & Data
+            name: 'Outfit',
+            cssVariable: '--font-outfit',
+            provider: fontProviders.google(),
+            weights: [300, 500], // Light y Medium
+            styles: ["normal"],
+            subsets: ["latin"],
+            fallbacks: ["sans-serif"],
         },
     ],
 });
