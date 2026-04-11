0fe36b24d7d78b167f4fa4677197e0859c18a751
Jhosua <168266147+Jhosua-Lascarro@users.noreply.github.com>
2026-04-11 12:26:38 -0500

    Revert "Chore/prettierrc only 20260411 (#10)"
    This reverts commit 158dd9b36efc190d1154e76fc70ba9d920d1412f.

--- README.md
+++
@@ -1,4 +0,0 @@
-# AgroChoco
-
-![Astro](https://img.shields.io/badge/Astro-6.1.3-FF5D01?style=for-the-badge&logo=astro&logoColor=white)
-![Tailwind CSS](https://img.shields.io/badge/Tailwind-4.2.2-38BDF8?style=for-the-badge&logo=tailwindcss&logoColor=white)
--- src/data/media.ts
+++
@@ -1,5 +0,0 @@
-import heroPlaceholder from "@images/ui/placeholder.png";
-
-export const globalPlaceholderImage = heroPlaceholder;
-export const globalPlaceholderImageSrc = heroPlaceholder.src;
-export const globalPlaceholderImageAlt = "Imagen referencial de AgroChoco";
