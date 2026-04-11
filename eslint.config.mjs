4e0729b2c4f41bb42b33ed0b97305e919980c55c
Jhosua <168266147+Jhosua-Lascarro@users.noreply.github.com>
2026-04-11 11:57:00 -0500

    chore: add prettier and eslint tooling for Astro

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [
+    {
+        ignores: ["dist/**", ".astro/**", "node_modules/**"]
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...eslintPluginAstro.configs["flat/recommended"],
+    {
+        files: ["**/*.astro"],
+        languageOptions: {
+            parserOptions: {
+                parser: tseslint.parser,
+                extraFileExtensions: [".astro"]
+            }
+        }
+    },
+    eslintConfigPrettier
+];
